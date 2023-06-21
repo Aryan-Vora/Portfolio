680f405b366a4d320347234f18a700e43fd98796
Aryan Vora <voraaryan1@gmail.com>
2023-06-20 17:01:44 -0700

    basic page (no projects)

--- app/page.js
+++ app/page.js
@@ -1,94 +1,58 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.topnav}>
+        <a rel="noopener" target="_self" href="#home">Home</a>
+        <a rel="noopener" target="_self" href="#about">About</a>
+        <a rel="noopener" target="_self" href="#projects">Projects</a>
+        <a rel="noopener" target="_self" href="mailto:voraaryan1@gmail.com">Contact</a>
+      </div>
+      <div id="home"className={styles.cardgenBox}>
+        
+        <div className={styles.boxContent}>
+            <h1>Full-Stack Web Developer</h1>
+            <p>Hey there! I'm Aryan Vora, a full-stack developer based in Santa Cruz, CA. </p>
+            <a href="https://github.com/Aryan-Vora"><img src="github-icon.svg"></img> </a>
+            <div className={styles.techStack}>
+            <ul>Tech Stack |
+              <li>
+                <img src="html-5.svg"></img>
+              </li>
+              <li>
+                <img src="css-3.svg"></img>
+              </li>
+              <li>
+                <img src="javascript.svg"></img>
+              </li>
+              <li>
+              <img src="react.svg"></img>
+              </li>
+              <li>
+              <img id="about" src="nextjs-icon.svg"></img>
+              </li>
+              </ul>
+              </div>
+        </div>
+        <img src="facecropped.png" className={styles.card}></img>
+      </div>
+      <hr ></hr>
+      <div className={styles.cardgenBox2}>
+      <img src="croppeddesk.jpg" className={styles.card2}></img>
+
+        <div className={styles.boxContent}>
+              <h2>About Me</h2>
+              <p className={styles.longDescription}>Motivated and accomplished computer science student with a passion for front-end development and a background in project leadership, hackathons, and tutoring. Seeking opportunities to leverage my skills in web development, and other technologies to contribute to innovative projects and make a positive impact in the field of software development.
+ </p>
         </div>
       </div>
+      <hr></hr>
+      <div id="projects"className={styles.cardgenBox3}>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.boxContent}>
+              <h2>Projects</h2>
+        </div>
       </div>
     </main>
   )
